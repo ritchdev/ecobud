3f0bf768763acf53107d5ebe974095e279c39a78
ritchdev <ritamch2016@gmail.com>
2025-12-29 01:28:47 +0530

    Fix minor UI bug in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
             <div className="h-full bg-[url(src/assets/hero.jpg)] bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center text-white text-center back p-[0px]">
              <div className="absolute inset-0 bg-black/50 z-0"></div>
                 <span className="flex">
-                    <h1 className="text-4xl font-bold pb-8 z-1">Welcome to</h1>
+                    <h1 className="text-5xl leading-tight font-bold pb-6 z-1">Welcome to</h1>
                     <img src="src/assets/wordfont-logo.png" alt="EcoBud" className="h-11 ml-3 translate-y-[8px] brightness-140 z-1"/>
                 </span>
                 <p className="text-lg z-1">Recycle today for a better tomorrow 🌱</p>
