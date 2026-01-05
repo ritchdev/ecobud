345e8c63de2b81ba773281d74b887e53567ff332
ritchdev <ritamch2016@gmail.com>
2026-01-06 01:10:37 +0530

    Bug Fix for Render

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -13,7 +13,7 @@ try {
     credential: firebaseAdmin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
   })
   });
   console.log("Firebase Admin Initialized");
